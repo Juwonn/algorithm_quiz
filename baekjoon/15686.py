1ed32c55ea36c46f1605bbe9349761ed841e65fc
Sprout_Juwon <49385400+Juwonn@users.noreply.github.com>
2020-08-18 20:49:42 +0900

    Update 15686.py

--- baekjoon/15686.py
+++ baekjoon/15686.py
@@ -2,7 +2,7 @@
 # Brute force
 # 그래프에서 치킨집과 집의 좌표를 배열에 저장 한다.
 # 폐업시키지 않을 치킨집이 최대 M개 이므로
-# M개의 좌표를 담은 치킨집의 조합 배열을 구한다. 조합은 itertools.combinations(list,n)을 사용한다.
+# M개의 좌표를 담은 치킨집의 조합 배열을 구한다. 조합은 itertools.combinations(list,M)을 사용한다.
 # 각 조합마다 집과의 최소거리를 계산함.
 # 반복문을 통해 최솟값을 구하는 문제이므로 초기 최솟값은 Python3 에서 정수의 최댓값인 sys.maxsize를 사용한다.
 
